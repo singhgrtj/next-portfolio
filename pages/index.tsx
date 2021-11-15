901a440bdecc397390c7b530c33e00f245e0b7a3
nomadedev2 <singhgrtj@gmail.com>
2021-11-15 14:58:27 +0100

    Project setup

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+// Component import
+import React from 'react';
+
+// Style and constants import
+
+// Other imports
+
+interface Props {}
+
+const Home: React.FC<Props> = (props) => {
+  const {} = props;
+
+  return (
+    <div className="h-screen w-full flex justify-center items-center font-body">
+      <h1 className="text-3xl font-semibold">Next Portfolio</h1>
+    </div>
+  );
+}
+
+export default Home;
